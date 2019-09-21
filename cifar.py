efb7759a8327141a8d45803b633b3f0e07f441b3
tian <t18215595124@gmail.com>
2019-09-21 19:53:14 +0800

    add resnet_ws

--- cifar.py
+++ cifar.py
@@ -254,7 +254,6 @@ def train(trainloader, model, criterion, optimizer, epoch, use_cuda):
         if use_cuda:
             inputs, targets = inputs.cuda(), targets.cuda(async=True)
         inputs, targets = torch.autograd.Variable(inputs), torch.autograd.Variable(targets)
-        print('inputs shape',inputs.shape)
         # compute output
         outputs = model(inputs)
 
@@ -263,8 +262,8 @@ def train(trainloader, model, criterion, optimizer, epoch, use_cuda):
         # measure accuracy and record loss
         prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
         losses.update(loss.data, inputs.size(0))
-        top1.update(prec1[0], inputs.size(0))
-        top5.update(prec5[0], inputs.size(0))
+        top1.update(prec1, inputs.size(0))
+        top5.update(prec5, inputs.size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
