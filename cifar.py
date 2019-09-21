0313f44094d2f206123dec751cd41ef7ef36dc2d
tian <t18215595124@gmail.com>
2019-09-21 19:40:52 +0800

    add resnet_ws

--- cifar.py
+++ cifar.py
@@ -258,7 +258,7 @@ def train(trainloader, model, criterion, optimizer, epoch, use_cuda):
         # compute output
         outputs = model(inputs)
         print('outputs:',outputs[:10,:])
-        print('targets:',targets[:10,:])
+        print('targets:',targets[:10])
         loss = criterion(outputs, targets)
         print('loss shape',loss.shape)
         # measure accuracy and record loss
