6c2dad01c43230255eefe09616b1ddcb077ea2b9
tian <t18215595124@gmail.com>
2019-09-21 16:20:37 +0800

    add resnet_ws

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,6 +7,5 @@ from .eval import *
 
 # progress bar
 import os, sys
-print(os.path.join(os.path.dirname(__file__), "progress"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "progress"))
-from progress.bar import Bar as Bar
+from utils.progress.progress.bar import Bar as Bar
