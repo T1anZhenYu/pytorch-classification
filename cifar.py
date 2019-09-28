09ef60933b2ed92ef303e3c64f1cd05f2bb7cc4b
tian <t18215595124@gmail.com>
2019-09-28 17:15:39 +0800

    add flip

--- cifar.py
+++ cifar.py
@@ -178,6 +178,12 @@ def main():
                     depth=args.depth,
                     block_name=args.block_name,
                 )
+    elif args.arch.endswith('resnet_ws_flip'):
+        model = models.__dict__[args.arch](
+                    num_classes=num_classes,
+                    depth=args.depth,
+                    block_name=args.block_name,
+                )
     else:
         model = models.__dict__[args.arch](num_classes=num_classes)
 
