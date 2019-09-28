8d957f620740c21feec2467d6770582bdae1aad9
tian <t18215595124@gmail.com>
2019-09-28 20:37:55 +0800

    add flip

--- models/layers.py
+++ models/layers.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn.parameter import Parameter
 from torch.nn import functional as F
-
+import math
 
 class Conv2d(nn.Conv2d):
 
@@ -22,25 +22,6 @@ class Conv2d(nn.Conv2d):
         return F.conv2d(x, weight, self.bias, self.stride,
                         self.padding, self.dilation, self.groups)
 
-class Conv2d_flip(nn.Conv2d):
-
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True):
-        super(Conv2d_flip, self).__init__(in_channels, out_channels, kernel_size, stride,
-                                     padding, dilation, groups, bias)
-
-    def forward(self, x):
-        # return super(Conv2d, self).forward(x)
-        weight = self.weight
-        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
-                                                            keepdim=True).mean(dim=3, keepdim=True)
-        weight = weight - weight_mean
-        std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
-        weight = weight / std.expand_as(weight)
-        return F.conv2d(x, weight, self.bias, self.stride,
-                        self.padding, self.dilation, self.groups)
-
-
 class Conv2d_new(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
                  padding=0, dilation=1, groups=1, bias=True):
@@ -55,7 +36,7 @@ class Conv2d_new(nn.Conv2d):
     def forward(self, x):  # return super(Conv2d, self).forward(x)
         weight = self.weight
         weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
-                                                            keepdim=True).mean(dim=3, keepdim=True)
+                                                    keepdim=True).mean(dim=3, keepdim=True)
         weight = weight - weight_mean
 
         out1 = F.conv2d(x, weight, self.bias, self.stride,
@@ -64,7 +45,8 @@ class Conv2d_new(nn.Conv2d):
         shape_2d = (1,out1.shape[1],1, 1)
         mu = torch.mean(out1, dim=(0, 2, 3)).view(shape_2d)
         var = torch.transpose(torch.mean(
-            (out1 - mu) ** 2, dim=(0, 2, 3)).view(shape_2d), 0, 1) # biased
+            (out1 - mu) ** 2, dim=(0, 2, 3)).view(shape_2d), 0, 1)*\
+              (2*torch.sqrt(2*math.pi)) # biased
         self.moving_var = nn.Parameter(self.momente*self.moving_var +
                                        (1-self.momente)*var,requires_grad=False)
         return F.conv2d(x, (1/(1+torch.abs(self.alpha)))*weight /torch.sqrt(self.moving_var + eps), \
