b80c4bbf1b749b9af19911931b1337ed954cc62f
tian <t18215595124@gmail.com>
2019-09-29 17:13:22 +0800

    change var

--- models/layers.py
+++ models/layers.py
@@ -45,7 +45,7 @@ class Conv2d_new(nn.Conv2d):
         shape_2d = (1,out1.shape[1],1, 1)
         mu = torch.mean(out1, dim=(0, 2, 3)).view(shape_2d)
         var = torch.transpose(torch.mean(
-            (out1 - mu) ** 2, dim=(0, 2, 3)).view(shape_2d), 0, 1)*4
+            (out1 - mu) ** 2, dim=(0, 2, 3)).view(shape_2d), 0, 1)
         self.moving_var = nn.Parameter(self.momente*self.moving_var +
                                        (1-self.momente)*var,requires_grad=False)
         return F.conv2d(x, (weight /torch.sqrt(self.moving_var + eps), \
