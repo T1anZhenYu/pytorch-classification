559743a6d5cdadc97edb2aa9c8b994439ce08c05
tian <t18215595124@gmail.com>
2019-10-05 19:34:56 +0800

    modify resnet

--- models/layers.py
+++ models/layers.py
@@ -40,14 +40,14 @@ class Conv2d_new(nn.Conv2d):
 
         total_num = x.shape[0] * x.shape[1] * x.shape[2] *x.shape[3]
         estimate_max = (0.82 * torch.log(torch.tensor(total_num,dtype= torch.float))).cuda()
-        real_max = torch.max(torch.max(torch.max(x,dim=0)[0],dim=-1)[0],dim=-1)[0].cuda()
+        real_max = torch.max(x).cuda()
 
         out1 = F.conv2d(x, weight, self.bias, self.stride,
                         self.padding, self.dilation, self.groups)
         shape_2d = (1,out1.shape[1],1, 1)
         estimate_mean = (torch.sqrt(torch.tensor(math.pi/2)).cuda()/\
                         (estimate_max/real_max+self.eps)).view([1,x.shape[1],1,1]).cuda()
-        x = (x - estimate_mean).cuda()
+        x = (x - torch.mean(x)).cuda()
         weight = weight - weight_mean
 
 
