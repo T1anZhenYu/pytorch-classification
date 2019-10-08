17df545a38ef4f3766dd47f0307c380becae0fbc
tian <t18215595124@gmail.com>
2019-10-08 19:38:39 +0800

    add resnet mybn

--- cifar.py
+++ cifar.py
@@ -190,6 +190,12 @@ def main():
                     depth=args.depth,
                     block_name=args.block_name,
                 )
+    elif args.arch.endswith('resnet_offical_GN'):
+        model = models.__dict__[args.arch](
+                    num_classes=num_classes,
+                    depth=args.depth,
+                    block_name=args.block_name,
+                )
     else:
         model = models.__dict__[args.arch](num_classes=num_classes)
 
--- models/cifar/resnet_offical_GN.py
+++ models/cifar/resnet_offical_GN.py
@@ -13,7 +13,7 @@ import math
 import numpy as np
 import time
 from .. import layers as L
-__all__ = ['resnet_mybn']
+__all__ = ['resnet_offical_GN']
 
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
@@ -97,10 +97,10 @@ class Bottleneck(nn.Module):
         return out
 
 
-class ResNet_MYBN(nn.Module):
+class ResNet_offcial_GN(nn.Module):
 
     def __init__(self, depth, num_classes=1000, block_name='BasicBlock'):
-        super(ResNet_MYBN, self).__init__()
+        super(ResNet_offcial_GN, self).__init__()
         # Model type specifies number of layers for CIFAR-10 model
         if block_name.lower() == 'basicblock':
             assert (depth - 2) % 6 == 0, 'When use basicblock, depth should be 6n+2, e.g. 20, 32, 44, 56, 110, 1202'
@@ -166,8 +166,8 @@ class ResNet_MYBN(nn.Module):
         return x
 
 
-def resnet_mybn(**kwargs):
+def resnet_offical_GN(**kwargs):
     """
     Constructs a ResNet model.
     """
-    return ResNet_MYBN(**kwargs)
+    return ResNet_offcial_GN(**kwargs)
