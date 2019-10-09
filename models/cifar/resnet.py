1ee06a79571dc1359f0077d7f4374a3335e08159
tian <t18215595124@gmail.com>
2019-10-09 14:51:57 +0800

    add resnet mybn

--- models/cifar/resnet.py
+++ models/cifar/resnet.py
@@ -48,7 +48,7 @@ class BasicBlock(nn.Module):
 
         out3 = self.bn2(out2)
 
-        if self.iter[0] % 100 == 0 and self.conv1.in_channels == 16 and self.conv1.out_channels == 32:
+        if self.iter[0] % 200 == 0 and self.conv1.in_channels == 16 and self.conv1.out_channels == 32:
             dic['input'] = out1.detach().cpu().numpy()
             dic['beforeBN'] = out2.detach().cpu().numpy()
             dic['afterBN'] = out3.detach().cpu().numpy()
