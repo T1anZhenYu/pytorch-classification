eed0b296a45eb3a9106e10059b906b8a8d918309
tian <t18215595124@gmail.com>
2019-10-09 14:44:33 +0800

    add resnet mybn

--- models/cifar/resnet.py
+++ models/cifar/resnet.py
@@ -37,7 +37,7 @@ class BasicBlock(nn.Module):
         self.iter = nn.Parameter(torch.ones([1]),requires_grad=False)
 
     def forward(self, x):
-        print(self.iter)
+
         residual = x
         dic = {}
         out = self.conv1(x)
@@ -48,7 +48,7 @@ class BasicBlock(nn.Module):
 
         out3 = self.bn2(out2)
 
-        if self.iter % 100 == 0 and self.conv1.in_channels == 16 and self.conv1.out_channels == 32:
+        if self.iter[0] % 100 == 0 and self.conv1.in_channels == 16 and self.conv1.out_channels == 32:
             dic['input'] = out1.detach().cpu().numpy()
             dic['beforeBN'] = out2.detach().cpu().numpy()
             dic['afterBN'] = out3.detach().cpu().numpy()
