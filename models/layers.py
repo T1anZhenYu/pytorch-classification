3a6c9a493a1eda6047d7dadc5145a3d21bff80f8
tian <t18215595124@gmail.com>
2019-10-20 16:53:23 +0800

    add resnet staticbn

--- models/layers.py
+++ models/layers.py
@@ -51,7 +51,7 @@ class MyStaticBatchNorm(nn.Module):
         self.running_mean = nn.Parameter(torch.zeros([1,self.num_features,1,1]))
 
         self.running_var = nn.Parameter(torch.ones([1, self.num_features, 1, 1]))
-        self.alpha = 1
+        self.running_alpha = nn.Parameter(torch.ones([1]),requires_grad=False)
     def forward(self, x,last_layer_weight,last_layer_input):
         c_in = last_layer_input.shape[1]
         weight_mean = torch.mean(last_layer_weight,(1,2,3))
@@ -63,24 +63,28 @@ class MyStaticBatchNorm(nn.Module):
                                        last_layer_input.shape[2] * \
                                        last_layer_input.shape[3])
         # alpha = self.alpha * self.momente + (1-self.momente) * real_max / estimate_max
-        alpha = real_max / estimate_max
+
         # real_mean = torch.mean(x,(0,2,3)).view([1,self.num_features,1,1])
         if self.residual:
             estimate_mean = (c_in * math.sqrt(math.pi / 2) * weight_mean*2)\
                 .view([1,self.num_features,1,1])
-            estimate_var = (alpha ** 2 * c_in ** 2 * math.pi / 2 * weight_var*4)\
+            estimate_var = (c_in ** 2 * math.pi / 2 * weight_var*4)\
                 .view([1,self.num_features,1,1])
         else:
             estimate_mean = (c_in * math.sqrt(math.pi / 2) * weight_mean)\
                 .view([1,self.num_features,1,1])
-            estimate_var = (alpha ** 2 * c_in ** 2 * math.pi / 2 * weight_var)\
+            estimate_var = (c_in ** 2 * math.pi / 2 * weight_var)\
                 .view([1,self.num_features,1,1])
         if self.training:
+            alpha = real_max / estimate_max
+            self.running_alpha = nn.Parameter(self.momente * self.running_alpha + \
+                                    (1 - self.momente) * alpha,requires_grad= False)
             self.running_mean = nn.Parameter(self.momente*self.running_mean +\
-                                             (1-self.momente)*estimate_mean)
+                                             (1-self.momente)* estimate_mean)
             self.running_var = nn.Parameter(self.running_var*self.momente + \
-                                            (1-self.momente)* estimate_var)
-            return (x - self.running_mean)/torch.sqrt(self.running_var+self.eps)
+                                            (1-self.momente)* alpha ** 2 * estimate_var)
+            return (x - estimate_mean)/torch.sqrt(self.running_alpha**2 * estimate_var \
+                                                  + self.eps)
         else :
             # temp_estimate_mean = self.running_mean*self.momente + \
             #                      (1-self.momente)*estimate_mean
