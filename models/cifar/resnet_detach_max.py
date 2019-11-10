209a69b688196feaa31d9ca120ce2f9470678fe6
tian <t18215595124@gmail.com>
2019-11-10 19:18:25 +0800

    add detach max

--- models/cifar/resnet_detach_max.py
+++ models/cifar/resnet_detach_max.py
@@ -7,25 +7,16 @@ and
 https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
 (c) YANG, Wei
 '''
-import torch
 import torch.nn as nn
 import math
 from .. import layers as L
-import numpy as np
-import time
 
 __all__ = ['resnet_detach_max']
 
-
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
-    return L.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                    padding=1, bias=False)
-
-
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return L.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
 
 
 class BasicBlock(nn.Module):
@@ -40,29 +31,23 @@ class BasicBlock(nn.Module):
         self.bn2 = L.Detach_max()
         self.downsample = downsample
         self.stride = stride
-        self.iter = nn.Parameter(torch.zeros(1), requires_grad=False)
 
     def forward(self, x):
         residual = x
-        dic = {}
+
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-        dic['iter'] = self.iter.cpu().numpy()
-        dic['input'] = out.detach().cpu().numpy()
+
         out = self.conv2(out)
-        dic['weight'] = self.conv2.weight.detach().cpu().numpy()
-        dic['beforeBN'] = out.detach().cpu().numpy()
         out = self.bn2(out)
-        dic['afterBN'] = out.detach().cpu().numpy()
-        if self.iter % 50 == 0 and self.conv2.in_channels == 16:
-            np.savez(str(time.time()) + ".npz", **dic)
+
         if self.downsample is not None:
             residual = self.downsample(x)
 
         out += residual
         out = self.relu(out)
-        self.iter = nn.Parameter(self.iter + 1, requires_grad=False)
+
         return out
 
 
@@ -71,32 +56,26 @@ class Bottleneck(nn.Module):
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
-        self.conv1 = conv1x1(inplanes, planes)
-
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn1 = L.Detach_max()
-        self.conv2 = L.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                              padding=1, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
         self.bn2 = L.Detach_max()
-
-        self.conv3 = conv1x1(planes, planes * 4, )
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
         self.bn3 = L.Detach_max()
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
 
     def forward(self, x):
-        dic = {}
-
         residual = x
 
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-        dic['input'] = out.detach().cpu().numpy()
+
         out = self.conv2(out)
-        dic['beforeBN'] = out.detach().cpu().numpy()
         out = self.bn2(out)
-        dic['afterBN'] = out.detach().cpu().numpy()
         out = self.relu(out)
 
         out = self.conv3(out)
@@ -107,14 +86,14 @@ class Bottleneck(nn.Module):
 
         out += residual
         out = self.relu(out)
-        np.savez(str(time.time()) + ".npz", **dic)
+
         return out
 
 
-class ResNet_Detach_Max(nn.Module):
+class ResNet_Detach_max(nn.Module):
 
     def __init__(self, depth, num_classes=1000, block_name='BasicBlock'):
-        super(ResNet_Detach_Max, self).__init__()
+        super(ResNet_Detach_max, self).__init__()
         # Model type specifies number of layers for CIFAR-10 model
         if block_name.lower() == 'basicblock':
             assert (depth - 2) % 6 == 0, 'When use basicblock, depth should be 6n+2, e.g. 20, 32, 44, 56, 110, 1202'
@@ -127,9 +106,10 @@ class ResNet_Detach_Max(nn.Module):
         else:
             raise ValueError('block_name shoule be Basicblock or Bottleneck')
 
+
         self.inplanes = 16
-        self.conv1 = L.Conv2d(3, 16, kernel_size=3, padding=1,
-                              bias=False)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1,
+                               bias=False)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
         self.layer1 = self._make_layer(block, 16, n)
@@ -139,7 +119,7 @@ class ResNet_Detach_Max(nn.Module):
         self.fc = nn.Linear(64 * block.expansion, num_classes)
 
         for m in self.modules():
-            if isinstance(m, L.Conv2d):
+            if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
             elif isinstance(m, nn.BatchNorm2d):
@@ -150,9 +130,9 @@ class ResNet_Detach_Max(nn.Module):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                L.Conv2d(self.inplanes, planes * block.expansion,
-                         kernel_size=1, stride=stride, bias=False),
-
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                
             )
 
         layers = []
@@ -164,13 +144,9 @@ class ResNet_Detach_Max(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        dic = {}
-
         x = self.conv1(x)
-
         x = self.bn1(x)
-
-        x = self.relu(x)  # 32x32
+        x = self.relu(x)    # 32x32
 
         x = self.layer1(x)  # 32x32
         x = self.layer2(x)  # 16x16
@@ -185,6 +161,6 @@ class ResNet_Detach_Max(nn.Module):
 
 def resnet_detach_max(**kwargs):
     """
-    Constructs a ResNet_WS model.
+    Constructs a ResNet model.
     """
-    return ResNet_Detach_Max(**kwargs)
+    return ResNet_Detach_max(**kwargs)
